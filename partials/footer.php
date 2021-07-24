5cba5a640886f6c365adb724d2060a35e7e3ea15
Md. Ammad Ali <mdammad75@gmail.com>
2021-07-24 14:17:18 +0600

    html file

---
+++ partials/footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
+</div>
+<script src="assets/plugins/jquery/jquery.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/adminlte.min.js"></script>
+</body>
+
+</html>
